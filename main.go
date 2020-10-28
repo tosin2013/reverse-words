868affe3a3207d7f9ec60904db5ea59c77adec3e
Tosin Akinosho <takinosh@redhat.com>
2020-10-28 15:20:35 +0000

    Release updated to v0.0.33

--- main.go
+++ main.go
@@ -40,7 +40,7 @@ var (
 	)
 )
 
-var version = "v0.0.32"
+var version = "v0.0.33"
 
 // ReturnRelease returns the release configured by the user
 func ReturnRelease(w http.ResponseWriter, r *http.Request) {
